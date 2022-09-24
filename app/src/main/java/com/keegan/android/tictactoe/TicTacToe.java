9a1c83973d56623c9df2165b8549158b2e92d847
Keegan <kgm1328@live.missouristate.edu>
2022-09-24 14:42:32 -0500

    Finished TicTacToe.java implementation

--- app/src/main/java/com/keegan/android/tictactoe/TicTacToe.java
+++ app/src/main/java/com/keegan/android/tictactoe/TicTacToe.java
@@ -12,7 +12,7 @@ public class TicTacToe {
 
     public int play(int row, int col){
         int currentTurn = turn;
-        if(row >= 0 && col >0 && row < SIDE && col < SIDE && game[row][col] == 0){
+        if(row >= 0 && col >= 0 && row < SIDE && col < SIDE && game[row][col] == 0){
             game[row][col] = turn;
             if(turn == 1){
                 turn = 2;
@@ -96,5 +96,17 @@ public class TicTacToe {
             }
         }
         turn = 1;
+    }
+
+    public String result(){
+        if(whoWon() > 0){
+            return "Player " + whoWon() + " won!";
+        }
+        else if(canNotPlay()){
+            return "Tie Game";
+        }
+        else{
+            return "Play!";
+        }
     }
 }
